fa46aca145fa51d4c5fc6b3978a87feec936d676
Micooz <micooz@hotmail.com>
2017-07-10 07:30:03 +0800

    ss-base: extract parsing logic to parse({...})

--- src/presets/ss-base.js
+++ src/presets/ss-base.js
@@ -65,6 +65,57 @@ export default class SSBasePreset extends IPreset {
     }
   }
 
+  parse({buffer, fail}) {
+    if (buffer.length < 7) {
+      fail(`invalid length: ${buffer.length}`);
+      return null;
+    }
+
+    const atyp = buffer[0];
+
+    let addr; // string
+    let port; // number
+    let offset = 3;
+
+    switch (atyp) {
+      case ATYP_V4:
+        addr = ip.toString(buffer.slice(1, 5));
+        port = buffer.slice(5, 7).readUInt16BE(0);
+        offset += 4;
+        break;
+      case ATYP_V6:
+        if (buffer.length < 19) {
+          fail(`invalid length: ${buffer.length}`);
+          return null;
+        }
+        addr = ip.toString(buffer.slice(1, 17));
+        port = buffer.slice(17, 19).readUInt16BE(0);
+        offset += 16;
+        break;
+      case ATYP_DOMAIN:
+        const domainLen = buffer[1];
+        if (buffer.length < domainLen + 4) {
+          fail(`invalid length: ${buffer.length}`);
+          return null;
+        }
+        addr = buffer.slice(2, 2 + domainLen).toString();
+        if (!isValidHostname(addr)) {
+          fail(`addr=${addr} is an invalid hostname`);
+          return null;
+        }
+        port = buffer.slice(2 + domainLen, 4 + domainLen).readUInt16BE(0);
+        offset += (domainLen + 1);
+        break;
+      default:
+        fail(`invalid atyp: ${atyp}`);
+        return null;
+    }
+    const data = buffer.slice(offset);
+    return {atyp, addr, port, data};
+  }
+
+  // tcp
+
   clientOut({buffer}) {
     if (!this._isHandshakeDone) {
       this._isHandshakeDone = true;
@@ -83,61 +134,21 @@ export default class SSBasePreset extends IPreset {
   serverIn({buffer, next, broadcast, fail}) {
     if (!this._isHandshakeDone) {
 
-      // shadowsocks aead cipher put [atyp][dst.addr][dst.port] into the first chunk
+      // shadowsocks(python) aead cipher put [atyp][dst.addr][dst.port] into the first chunk
       // we must wait onConnected() before next().
       if (this._isAddressReceived) {
         this._staging = Buffer.concat([this._staging, buffer]);
         return;
       }
 
-      if (buffer.length < 7) {
-        fail(`invalid length: ${buffer.length}`);
+      const parseResult = this.parse({buffer, fail});
+      if (parseResult === null) {
         return;
       }
 
-      const atyp = buffer[0];
+      const {atyp, addr, port, data} = parseResult;
 
-      let addr; // string
-      let port; // number
-      let offset = 3;
-
-      switch (atyp) {
-        case ATYP_V4:
-          addr = ip.toString(buffer.slice(1, 5));
-          port = buffer.slice(5, 7).readUInt16BE(0);
-          offset += 4;
-          break;
-        case ATYP_V6:
-          if (buffer.length < 19) {
-            fail(`invalid length: ${buffer.length}`);
-            return;
-          }
-          addr = ip.toString(buffer.slice(1, 17));
-          port = buffer.slice(17, 19).readUInt16BE(0);
-          offset += 16;
-          break;
-        case ATYP_DOMAIN:
-          const domainLen = buffer[1];
-          if (buffer.length < domainLen + 4) {
-            fail(`invalid length: ${buffer.length}`);
-            return;
-          }
-          addr = buffer.slice(2, 2 + domainLen).toString();
-          if (!isValidHostname(addr)) {
-            fail(`addr=${addr} is an invalid hostname`);
-            return;
-          }
-          port = buffer.slice(2 + domainLen, 4 + domainLen).readUInt16BE(0);
-          offset += (domainLen + 1);
-          break;
-        default:
-          fail(`invalid atyp: ${atyp}`);
-          return;
-      }
-
-      const data = buffer.slice(offset);
-
-      // notify to connect to the real server
+      // notify socket to connect to the destination
       broadcast({
         type: SOCKET_CONNECT_TO_DST,
         payload: {
@@ -158,6 +169,39 @@ export default class SSBasePreset extends IPreset {
     } else {
       return buffer;
     }
+  }
+
+  // udp
+
+  clientOutUdp({buffer}) {
+    return Buffer.from([
+      this._atyp,
+      ...(this._atyp === ATYP_DOMAIN) ? numberToBuffer(this._host.length, 1) : [],
+      ...this._host,
+      ...this._port,
+      ...buffer
+    ]);
+  }
+
+  serverInUdp({buffer, next, broadcast, fail}) {
+    const parseResult = this.parse({buffer, fail});
+    if (parseResult === null) {
+      return;
+    }
+    const {atyp, addr, port, data} = parseResult;
+    // notify socket to connect to the destination
+    broadcast({
+      type: SOCKET_CONNECT_TO_DST,
+      payload: {
+        targetAddress: {
+          type: atyp,
+          host: addr,
+          port
+        },
+        // once connected
+        onConnected: () => next(data)
+      }
+    });
   }
 
 }
